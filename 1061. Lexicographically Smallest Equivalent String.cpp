2a43f32dc0818d5cb06f2488f787984eec62ca9f
ABHINAV <78312043+abhinav525@users.noreply.github.com>
2023-01-14 23:05:12 +0530

    Create 1061. Lexicographically Smallest Equivalent String.cpp

---
+++ 1061. Lexicographically Smallest Equivalent String.cpp
@@ -0,0 +1,54 @@
+//we disjoint set data structure int this 
+// in dsu we use two function -1->make union and 2-> is findparent..
+class DSU {
+public:
+    vector<int>rank;
+    vector<int>parent;
+    
+    DSU(int n) {
+        rank.resize(n);
+        parent.resize(n);
+        for (int i = 0; i < n; i++) {
+            rank[i] = i;
+            parent[i] = i;
+        }
+    }
+    
+    int findParent(int x) {
+        if (x == parent[x]) {
+            return x;
+        }
+        return parent[x] = findParent(parent[x]);
+    }
+    
+    void makeUnion(int x, int y) {
+        int p1 = findParent(x), p2 = findParent(y);
+        if (p1 != p2) {
+            if (rank[p1] < rank[p2]) {
+                parent[p2] = p1;
+            }
+            else {
+                parent[p1] = p2;
+            }
+        }
+    }
+};
+
+class Solution {
+public:
+    string smallestEquivalentString(string s1, string s2, string baseStr) {
+        DSU dsu(26);
+        for(int i=0;i<s1.size();i++){
+            dsu.makeUnion(s1[i]-'a',s2[i]-'a');// to make thee rank of a -0
+        }
+        string ans;
+
+        for(int i=0;i<baseStr.size();i++){
+
+            char ch=dsu.findParent(baseStr[i]-'a')+'a';
+            ans+=ch;
+        }
+        return ans;
+        
+    }
+};
