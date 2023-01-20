029f949013153cabf0050e8fa0d199b3da34b6a0
ABHINAV <78312043+abhinav525@users.noreply.github.com>
2023-01-20 11:48:46 +0530

    Create 491. Non-decreasing Subsequences.java

---
+++ 491. Non-decreasing Subsequences.java
@@ -0,0 +1,23 @@
+class Solution {
+    int[] arr;
+    HashSet<List<Integer>> hashset=new HashSet<>();
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        arr=nums;
+        List<Integer>arrayList=new ArrayList<>();
+        recursion(arrayList,0);
+        List<List<Integer>> result=new ArrayList<>(hashset);
+        return result;
+        
+    }
+    public void recursion(List<Integer> arrayList,int index){
+        if(arrayList.size()>=2 ) hashset.add(new ArrayList(arrayList));
+        for(int i=index;i<arr.length;i++){
+            if(arrayList.size()==0 || arr[i]>=arrayList.get(arrayList.size()-1)){
+             arrayList.add(arr[i]);
+            recursion(arrayList,i+1);
+            arrayList.remove(arrayList.size()-1);
+            }
+            
+        }
+    }
+}
